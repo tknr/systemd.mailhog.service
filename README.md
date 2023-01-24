fa9fecd1fae5844cd995f8e95794afc7b2836299
tknr <geishagirlskoume@gmail.com>
2023-01-24 19:28:43 +0900

    u

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # systemd.mailhog.service
+
+thanks : https://gist.github.com/fuji44/4ba6e412e253309c8fddba4486e34e2e
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 cd `dirname $0`
+curl -s https://api.github.com/repos/mailhog/MailHog/releases/latest | jq -r '.assets[] | select(.name | test("MailHog_linux_amd64")) | .browser_download_url' | head -n 1 | xargs curl -LJO | exit 1
+chmod +x MailHog_linux_amd64 | exit 1
+mv MailHog_linux_amd64 /usr/local/bin/mailhog | exit 1
 cp -f mailhog.service /etc/systemd/system/
